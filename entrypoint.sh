4fd4bcaa4ea4325104e749d6522c918d75548ee8
luckydonald <luckydonald@flutterb.at>
2017-11-16 00:36:56 +0100

    [hey_admin] renamed test instance to hey_admin_test. Also uWSGI args.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-set -x;
+set -e;
+
+SOCKET_PATH="/sockets/bots/${URL_PATH}.sock";
+CMD="/usr/local/bin/uwsgi";
+ARGS="--ini /config/uwsgi.ini --need-app --die-on-term --socket=${SOCKET_PATH} --mount /${URL_PATH}=main.py $@ ${MORE_UWSGI_ARGS}";
+
+function cleanup {
+    # remove socket
+    echo 'Cleaning up!';
+    set -x;
+    rm "${SOCKET_PATH}";
+}
+trap cleanup EXIT;
 
 chown -R $USER_UID:$GROUP_UID /sockets/bots/;
-CMD="/usr/local/bin/uwsgi";
-ARGS="--ini /config/uwsgi.ini --need-app --die-on-term --socket=/sockets/bots/${URL_PATH}.sock --mount /${URL_PATH}=main.py $@";
-echo "exec gosu $USER_UID:$GROUP_UID $CMD ''$ARGS''";
-exec gosu $USER_UID:$GROUP_UID $CMD $ARGS
+echo "exec gosu $USER_UID:$GROUP_UID $CMD $ARGS";
+set -ex;
+exec gosu $USER_UID:$GROUP_UID $CMD $ARGS;
