5fc1070fc5cc03c4751d31f7a5115bb35969799a
luckydonald <mac2019.git@luckydonald.de>
2019-06-16 03:03:29 +0200

    [hey_admin] bumped versions to have the webhooks set automatically.

---
+++ requirements.py
@@ -0,0 +1,18 @@
+requirements = [
+    ## Web ##
+    "uwsgi",
+    "flask",
+
+    ## Bot ##
+    # teleflask==2.0.0  # telegram bot, flask-like!
+    "teleflask",
+
+    # pytgbot
+    "pytgbot",
+
+    # luckydonald-utils
+    "luckydonald-utils",
+
+    ## Diversa ##
+    "raven[flask]"  # debug
+]
